630016c64fc376116b584d0cae17b1fcbd346f80
S0ngHa0 <115171734+S0ngHa0@users.noreply.github.com>
2023-04-02 23:56:16 +0700

    Add project files.

---
+++ Web-Quang-ba-san-pham/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_Quang_ba_san_pham.MvcApplication" Language="C#" %>
---
+++ Web-Quang-ba-san-pham/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_Quang_ba_san_pham.Startup))]
+namespace Web_Quang_ba_san_pham
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
