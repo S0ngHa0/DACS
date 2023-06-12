423f6aed4c52904b8f8c8da5efe73eb68c883ef9
NhatHao <goldenvo397@gmail.com>
2023-06-13 01:07:15 +0700

    hhh

---
+++ Web-Quan-Ba-San-Pham/Web-Quan-Ba-San-Pham/Controllers/SanPhamController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web_Quan_Ba_San_Pham.Models.Models1;
+
+namespace Web_Quan_Ba_San_Pham.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        private Model1 db = new Model1();
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Partial_ItemsByCateId()
+        {
+            var items = db.tb_Sanpham.Where(x => x.Sp).Take(12).ToList();
+            return PartialView(items);
+        }
+    }
+}
